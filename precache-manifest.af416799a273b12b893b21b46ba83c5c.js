d33d9234f4dcb5181cdb9334a501d7e18798aa66
ishika1727 <ishikadubey2000@gmail.com>
2020-06-14 22:53:38 +0530

    Updates

---
+++ precache-manifest.af416799a273b12b893b21b46ba83c5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b987975e54f72b5a6893d8b88e8e4d5",
+    "url": "/my-portfolio-website/index.html"
+  },
+  {
+    "revision": "67093a630a53113f7179",
+    "url": "/my-portfolio-website/static/css/main.e035cf48.chunk.css"
+  },
+  {
+    "revision": "6f04cfed900e327027ed",
+    "url": "/my-portfolio-website/static/js/2.8644c8e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio-website/static/js/2.8644c8e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67093a630a53113f7179",
+    "url": "/my-portfolio-website/static/js/main.7a3b6f31.chunk.js"
+  },
+  {
+    "revision": "f6d56c40e7a10bd9ceff",
+    "url": "/my-portfolio-website/static/js/runtime-main.1b745dad.js"
+  },
+  {
+    "revision": "c6422a80fe41e37a6b7516fd8fd9d256",
+    "url": "/my-portfolio-website/static/media/back2.c6422a80.png"
+  },
+  {
+    "revision": "fa55eebdce3a8548fdefabb86371cb8f",
+    "url": "/my-portfolio-website/static/media/my.fa55eebd.png"
+  }
+]);
